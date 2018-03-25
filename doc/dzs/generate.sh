e4d1c07a906289c250b6364842a94b3da8c48aa5
Richard Szabo <richard.szabo@on-a-on.com>
2018-03-25 18:53:08 +0200

    initial commit

---
+++ doc/dzs/generate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+asciidoctor -T ~/repos/asciidoctor-backends/slim/dzslides/ -E slim enum_class.adoc
---
+++ doc/reveal/generate.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf enum_class.html
+
+bundle exec asciidoctor-revealjs enum_class.adoc
+
